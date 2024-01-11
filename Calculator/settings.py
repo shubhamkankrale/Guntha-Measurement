156536fab1ccd9922aa33bf85ad88444d9fb2d4f
shubhamkankrale <130346624+shubhamkankrale@users.noreply.github.com>
2024-01-11 23:19:12 +0530

    Update settings.py

--- Calculator/settings.py
+++ Calculator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7bgre$+_#qz79u%zb4vhlgkwt+x9)ab4al+vf$wlb_2rhbnj1p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["guntha.onrender.com"]
 
